caf526c79a28fa0f3c38737e529f4b95a0ebdb3c
Raul Galuppo <raul.i.galuppo@gmail.com>
2017-10-06 20:18:28 -0300

    change sms plugin. Works on Android.

--- www/js/testSMS.js
+++ www/js/testSMS.js
@@ -1,12 +1,10 @@
 var testSMS = {
-	smsList: [],
    interceptEnabled: false,
 	test: function() {
 		testSMS.init();
 		var to = "123456789";
 		var message = "This is an SMS message test";
 		testSMS.sendSMS(to, message);
-		testSMS.listSMS();
 	},
 	init: function(){
 		document.addEventListener('onSMSArrive', function(e){
@@ -16,42 +14,26 @@ var testSMS = {
 			testSMS.onSuccess(JSON.stringify(data));
 		});
 	},
-	sendSMS: function(sendto, textmsg) {
-		if(sendto.indexOf(";") >=0) {
-			sendto = sendto.split(";");
-			for(i in sendto) {
-				sendto[i] = sendto[i].trim();
-			}
-		}
-		var error = function(err) {
-			testSMS.onError(err);
-      };
-      var success = function (msg) {
-      	testSMS.onSuccess('successfully message send!\n');
-      };
-		SMS.sendSMS(sendto, textmsg, success, error);
-	},
-	listSMS: function() {
-		var filter = {};
-		var success = function(data) {
-			var text = "";
-      	if(Array.isArray(data)) {
-      		for(var i=0; i < data.length; i++) {
-        			var sms = data[i];
-        			testSMS.smsList.push(sms);
-        			testSMS.smsList.push(sms);
-        			text += "number: " + sms.address +
-        				"\n msg: " + sms.body +
-        				"\n date send: " + new Date(sms.date_sent) + "\n";
-        		}
-        	}
-        	testSMS.onSuccess( text );
-     	};
-     	var error = function(err) {
-			testSMS.onError('error list sms: ' + err);
-      }; 
-		SMS.listSMS(filter, success, error);
+	sendSMS: function(number, message) {
+		var options = {
+            replaceLineBreaks: false, // true to replace \n by a new line, false by default
+            android: {
+                //intent: 'INTENT'  // send SMS with the native android SMS messaging
+                intent: '' // send SMS without open any other app
+            }
+          };
+
+		sms.send(number, message, options, testSMS.onSuccess, testSMS.onError);
    },
+     checkSMSPermission: function() {
+        var success = function (hasPermission) { 
+            if (hasPermission) {
+                testSMS.onSuccess("Have permission to send a SMS!\n");
+            } else
+               testSMS.onError("Have not permission!!\n");
+        };
+        sms.hasPermission(testSMS.onSuccess, testSMS.onError);
+    },
 	onError: function (messageError) {
 		utils.onError(messageError);
 	},
